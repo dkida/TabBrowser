60a994d74f0158625750197df6b6d4fa933f518a
dkida <danielkida@hotmail.com>
2020-02-19 22:27:58 +0100

    Updates

---
+++ precache-manifest.94507e247bbd13788b65d00d00eeada2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18bcb189a12beea8946293de2b6d5efe",
+    "url": "/TabBrowser/index.html"
+  },
+  {
+    "revision": "6a608a76885315f966ad",
+    "url": "/TabBrowser/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a36e52aedb4212bf24bf",
+    "url": "/TabBrowser/static/css/main.c7644b0a.chunk.css"
+  },
+  {
+    "revision": "6a608a76885315f966ad",
+    "url": "/TabBrowser/static/js/2.ce1d594d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/TabBrowser/static/js/2.ce1d594d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a36e52aedb4212bf24bf",
+    "url": "/TabBrowser/static/js/main.fb5f2c17.chunk.js"
+  },
+  {
+    "revision": "f7b48615f52f80fdec10",
+    "url": "/TabBrowser/static/js/runtime-main.e6aadd78.js"
+  }
+]);
